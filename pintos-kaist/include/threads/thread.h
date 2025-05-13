776b30b37654b2155c460dc0f6a87f695032228e
안유진 <yoojin.an0@gmail.com>
2025-05-14 00:36:33 +0900

    fix(thread.h): thread 멤버변수로 d_elem 추가

--- pintos-kaist/include/threads/thread.h
+++ pintos-kaist/include/threads/thread.h
@@ -99,6 +99,7 @@ struct thread {
 
 	/* Shared between thread.c and synch.c. */
 	struct list_elem elem;              /* List element. */
+	struct list_elem d_elem;            /* Donations List element. */
 
 #ifdef USERPROG
 	/* Owned by userprog/process.c. */
